416a30ea4ff2d721852dcc0fa5ba87419113eade
Omri Gazitt <ogazitt@gmail.com>
2020-08-03 14:15:01 -0700

    HN graphql client first commit

---
+++ src/components/Link.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Link({link}) {
+  return (
+    <div>
+      <div>
+        {link.description} ({link.url})
+      </div>
+    </div>
+  )
+}
+
+export default Link
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+// 1
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
