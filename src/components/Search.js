f4c9bf289202b37852caf6bf5a36d4bae2d4817f
Omri Gazitt <ogazitt@gmail.com>
2020-08-03 16:53:40 -0700

    added search to HN apollo client

---
+++ src/components/Search.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import { withApollo } from 'react-apollo'
+import gql from 'graphql-tag'
+import Link from './Link'
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+class Search extends Component {
+
+  state = {
+    links: [],
+    filter: ''
+  }
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_SEARCH_QUERY })
+  
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+  
+    store.writeQuery({ query: FEED_SEARCH_QUERY, data })
+  }  
+
+  render() {
+    return (
+      <div>
+        <div>
+          Search
+          <input
+            type='text'
+            onChange={e => this.setState({ filter: e.target.value })}
+          />
+          <button onClick={() => this._executeSearch()}>OK</button>
+        </div>
+        {this.state.links.map((link, index) => (
+          <Link 
+            key={link.id} 
+            link={link} 
+            index={index} 
+            updateStoreAfterVote={this._updateCacheAfterVote} 
+          />
+        ))}
+      </div>
+    )
+  }
+
+
+  _executeSearch = async () => {
+    const { filter } = this.state
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter },
+    })
+    const links = result.data.feed.links
+    this.setState({ links })
+  }
+}
+
+export default withApollo(Search)
